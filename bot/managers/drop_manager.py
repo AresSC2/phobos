8383df82aab83d775edd2b2bf0463640a5c17fc0
Tom Kerr <tomkerrdev@gmail.com>
2023-06-18 14:53:44 +0100

    feat: improve mine drop role assignment
    Removed the role assignment in the combat class to the manager

--- bot/managers/drop_manager.py
+++ bot/managers/drop_manager.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
+
+from sc2.position import Point2
 
 from ares import ManagerMediator
 from ares.consts import DROP_ROLES, UnitRole
@@ -15,6 +17,9 @@ if TYPE_CHECKING:
 
 
 class DropManager(Manager):
+    # at this percentage, medivac should be unassigned and go home
+    MIN_HEALTH_MEDIVAC_PERC: float = 0.2
+
     def __init__(
         self,
         ai: "AresBot",
@@ -54,7 +59,7 @@ class DropManager(Manager):
             self.ai.build_order_runner.chosen_opening == "OneOneOne"
             and not self._assigned_111_mine_drop
         ):
-            mine_drop_target = self.ai.enemy_start_locations[0].towards(
+            mine_drop_target: Point2 = self.ai.enemy_start_locations[0].towards(
                 self.ai.game_info.map_center, -4.0
             )
             unit_dict: dict[UnitID, Units] = self.manager_mediator.get_own_army_dict
@@ -62,20 +67,19 @@ class DropManager(Manager):
             if UnitID.MEDIVAC in unit_dict and (
                 UnitID.WIDOWMINE in unit_dict or UnitID.WIDOWMINEBURROWED in unit_dict
             ):
-                medivacs: Units = unit_dict[UnitID.MEDIVAC]
+                medivacs: Units = unit_dict[UnitID.MEDIVAC].filter(
+                    lambda m: m.health_percentage > self.MIN_HEALTH_MEDIVAC_PERC
+                )
                 mines: Units = self.ai.units(
                     {UnitID.WIDOWMINE, UnitID.WIDOWMINEBURROWED}
                 )
 
                 if len(medivacs) > 0 and len(mines) > 1:
+                    assign_role: Callable = self.manager_mediator.assign_role
                     medivac: Unit = unit_dict[UnitID.MEDIVAC][0]
-                    self.manager_mediator.assign_role(
-                        tag=medivac.tag, role=UnitRole.DROP_SHIP
-                    )
+                    assign_role(tag=medivac.tag, role=UnitRole.DROP_SHIP)
                     for u in mines:
-                        self.manager_mediator.assign_role(
-                            tag=u.tag, role=UnitRole.DROP_UNITS_TO_LOAD
-                        )
+                        assign_role(tag=u.tag, role=UnitRole.DROP_UNITS_TO_LOAD)
                     self._medivac_tag_to_mine_tracker[medivac.tag] = {
                         "mine_tags": {mine.tag for mine in mines},
                         "target": mine_drop_target,
@@ -83,24 +87,43 @@ class DropManager(Manager):
                     self._assigned_111_mine_drop = True
 
     def _unassign_drops(self) -> None:
-        # unassign units from mine drop if medivac or assigned mines have died
-        for med_tag, mine_tracker in self._medivac_tag_to_mine_tracker.items():
-            if medivac := self.ai.unit_tag_dict.get(med_tag, None):
-                if medivac.has_cargo:
-                    return
-                else:
-                    mines: list[Unit] = [
-                        m for m in self.ai.units if m.tag in mine_tracker["mine_tags"]
-                    ]
-                    if len(mines) == 0:
-                        self.manager_mediator.assign_role(
-                            tag=medivac.tag, role=UnitRole.DEFENDING
-                        )
-            # else no medivac:
-            # leave mines in enemy base as they tend to die trying to escape
+        self._unassign_mine_drops(switch_to=UnitRole.ATTACKING)
 
-    def _execute_drops(self):
+    def _execute_drops(self) -> None:
         self._mine_drops.execute(
             self.manager_mediator.get_units_from_roles(roles=DROP_ROLES),
             medivac_tag_to_mine_tracker=self._medivac_tag_to_mine_tracker,
         )
+
+    def _unassign_mine_drops(self, switch_to: UnitRole) -> None:
+        # unassign units from mine drop if medivac or assigned mines have died
+        for med_tag, mine_tracker in self._medivac_tag_to_mine_tracker.items():
+            if medivac := self.ai.unit_tag_dict.get(med_tag, None):
+                # deal with low health medivac, assign mines to drop attack
+                if medivac.health_percentage <= self.MIN_HEALTH_MEDIVAC_PERC:
+                    self.manager_mediator.assign_role(tag=medivac.tag, role=switch_to)
+                    self.manager_mediator.batch_assign_role(
+                        tags=mine_tracker["mine_tags"],
+                        role=UnitRole.DROP_UNITS_ATTACKING,
+                    )
+                else:
+                    # all looking good, leave alone
+                    if medivac.has_cargo:
+                        return
+                    # no cargo, are mines still around?
+                    else:
+                        mines: list[Unit] = [
+                            m
+                            for m in self.ai.units
+                            if m.tag in mine_tracker["mine_tags"]
+                        ]
+                        # no mines, assign medivac to something else
+                        if len(mines) == 0:
+                            self.manager_mediator.assign_role(
+                                tag=medivac.tag, role=switch_to
+                            )
+            # no medivac exists, ensure mines are left on attacking mode
+            else:
+                self.manager_mediator.batch_assign_role(
+                    tags=mine_tracker["mine_tags"], role=UnitRole.DROP_UNITS_ATTACKING
+                )
